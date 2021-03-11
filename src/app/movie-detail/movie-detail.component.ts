8cecba2f9a95a16b8a2a84865dccf8bd0b6f19c6
sandippote <sandippote0385@gmail.com>
2021-03-12 00:04:01 +0530

    1 file modified

--- src/app/movie-detail/movie-detail.component.ts
+++ src/app/movie-detail/movie-detail.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnDestroy, OnInit } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
+import { takeUntil } from "rxjs/internal/operators/takeUntil";
 import { Subject } from "rxjs/internal/Subject";
 import { MovieService } from "../app.service";
 
@@ -9,6 +10,8 @@ import { MovieService } from "../app.service";
   styleUrls: ["./movie-detail.component.scss"]
 })
 export class MovieDetailComponent implements OnInit, OnDestroy {
+  movieId: number;
+  detail: any;
   unsubscribe: Subject<any>;
 
   constructor(
@@ -18,7 +21,22 @@ export class MovieDetailComponent implements OnInit, OnDestroy {
     this.unsubscribe = new Subject();
   }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.movieId = this._activatedRoute.snapshot.params["id"];
+
+    if (this.movieId) {
+      this.getMovieDetail();
+    }
+  }
+
+  getMovieDetail() {
+    this._movieService
+      .getMovieDetail(this.movieId)
+      .pipe(takeUntil(this.unsubscribe))
+      .subscribe(res => {
+        console.log(res);
+      });
+  }
 
   ngOnDestroy(): void {
     this.unsubscribe.next();
