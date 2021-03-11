d2c390b4d74794ac990c52b2d1ca6811085d72ee
sandippote <sandippote0385@gmail.com>
2021-03-12 00:00:26 +0530

    1 file modified

--- src/app/movie-detail/movie-detail.component.ts
+++ src/app/movie-detail/movie-detail.component.ts
@@ -1,20 +1,27 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { MovieService } from '../app.service';
+import { Component, OnDestroy, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { Subject } from "rxjs/internal/Subject";
+import { MovieService } from "../app.service";
 
 @Component({
-  selector: 'app-movie-detail',
-  templateUrl: './movie-detail.component.html',
-  styleUrls: ['./movie-detail.component.scss']
+  selector: "app-movie-detail",
+  templateUrl: "./movie-detail.component.html",
+  styleUrls: ["./movie-detail.component.scss"]
 })
-export class MovieDetailComponent implements OnInit {
+export class MovieDetailComponent implements OnInit, OnDestroy {
+  unsubscribe: Subject<any>;
 
   constructor(
     private _activatedRoute: ActivatedRoute,
     private _movieService: MovieService
-  ) { }
-
-  ngOnInit() {
+  ) {
+    this.unsubscribe = new Subject();
   }
 
+  ngOnInit() {}
+
+  ngOnDestroy(): void {
+    this.unsubscribe.next();
+    this.unsubscribe.complete();
+  }
 }
